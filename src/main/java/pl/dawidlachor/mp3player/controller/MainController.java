376e33da5943f92141904df37e58e1b6354b3284
Dawid Lachor <dawidlachor@gmail.com>
2020-03-05 17:45:13 +0100

    Add communication between Controller

--- src/main/java/pl/dawidlachor/mp3player/controller/MainController.java
+++ src/main/java/pl/dawidlachor/mp3player/controller/MainController.java
@@ -1,9 +1,21 @@
 package pl.dawidlachor.mp3player.controller;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    ControlController controlPaneController;
+    @FXML
+    ContentController contentPaneController;
+    @FXML
+    MenuController menuPaneController;
 
     public void initialize() {
         System.out.println("Main controller created");
+        System.out.println(contentPaneController);
+        System.out.println(controlPaneController);
+        System.out.println(menuPaneController);
     }
 
 }
